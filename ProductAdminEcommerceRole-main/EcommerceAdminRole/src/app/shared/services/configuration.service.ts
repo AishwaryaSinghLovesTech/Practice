304265bfd9394bb9c5631f36bcd99855d3259eb8
Aishwarya <aishwaryasingh205@gmail.com>
2022-01-12 07:51:07 +0530

    Initial structure of app with dependencies

---
+++ ProductAdminEcommerceRole-main/EcommerceAdminRole/src/app/shared/services/configuration.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { PersistanceService } from './persistance.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConfigurationService {
+  env = "test"
+  constructor(public _persistance:PersistanceService) { 
+
+    _persistance.set("productUrl","assets/mock-json/products.json")
+
+  }
+}
