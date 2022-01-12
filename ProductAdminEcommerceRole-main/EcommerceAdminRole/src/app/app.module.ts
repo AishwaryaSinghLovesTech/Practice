7990e77eb87d4d3dd16ad02a2899f799b34d1a09
Aishwarya <aishwaryasingh205@gmail.com>
2022-01-12 08:44:53 +0530

    generated req. components

--- ProductAdminEcommerceRole-main/EcommerceAdminRole/src/app/app.module.ts
+++ ProductAdminEcommerceRole-main/EcommerceAdminRole/src/app/app.module.ts
@@ -6,17 +6,22 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './shared/material/material.module';
-
+import { HttpClientModule } from '@angular/common/http';
+import { ProductAdminComponent } from './product-admin/product-admin.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductAdminComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     SharedModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
